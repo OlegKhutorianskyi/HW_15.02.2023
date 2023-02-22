0cd8c878e071983d724bd24365c3381130f5e332
Oleg Khutorianskiy <compilated510@outlook.com>
2023-02-22 20:02:18 +0100

    deleteComment

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,6 @@
 import { createStore } from 'redux';
 import { userReducer } from './reducer/userReducer';
 
-// const rootReducer = combineReducers({
-//     user: userReducer
-// })
-
 export  const store = createStore(userReducer)
 
 
